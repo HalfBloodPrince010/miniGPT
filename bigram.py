fe70ac72b3e3c5569f43c50fd981f5cfa0d88035
Prashanth Pujar <prashanth.pujar.usc@gmail.com>
2025-04-16 18:18:57 -0700

    Add Bigram Model, and logic for Training
    **Testing**
    
    python3 bigram.py
    torch.Size([1115394])
    step 0: train loss 4.6388, val loss 4.6304
    step 300: train loss 4.2973, val loss 4.2924
    step 600: train loss 3.9957, val loss 3.9889
    step 900: train loss 3.7469, val loss 3.7294
    step 1200: train loss 3.5196, val loss 3.5197
    step 1500: train loss 3.3264, val loss 3.3299
    step 1800: train loss 3.1715, val loss 3.1812
    step 2100: train loss 3.0404, val loss 3.0386
    step 2400: train loss 2.9386, val loss 2.9403
    step 2700: train loss 2.8444, val loss 2.8595
    step 3000: train loss 2.7707, val loss 2.7801
    step 3300: train loss 2.7262, val loss 2.7249
    step 3600: train loss 2.6784, val loss 2.6799
    step 3900: train loss 2.6393, val loss 2.6558
    step 4200: train loss 2.6116, val loss 2.6287
    step 4500: train loss 2.5856, val loss 2.5972
    step 4800: train loss 2.5739, val loss 2.5788
    step 5100: train loss 2.5484, val loss 2.5630
    step 5400: train loss 2.5328, val loss 2.5525
    step 5700: train loss 2.5250, val loss 2.5473
    
    // Generated Sample
    
    Awlie: or:
    Sty,
    S: dlly w bourn RIZ-OMamyofove, t heres tJIncasQfub3Wh at,
    ABore h Capefou.
    
    I Joun t f that. ; micatio'To banTHLed,
    qvowharcigss,.
    ATLINRCagusorQjofolpisparde; I be was I ke e wn IUSShacWhe let; h s rinisares thald th tr y:
    
    D:
    
    N att on four WUSis ace dGowire, mound IAfit gde
    F:
    tUnelld INon?
    TFuse read-US:
    
    Beat hingn.
    
    KI un:
    NLINT: aly I nthe trvepo f gs thatyoure y OUeshilth hit o paBul ltigUSat anonth wo ou?
    Themusignidend cy thag h withepomenss, t thif gnd,
    ann rofefang,

--- bigram.py
+++ bigram.py
@@ -5,43 +5,55 @@ from torch.nn import functional as F
 
 torch.manual_seed(1996)
 
+##########################
+#
+# HYPERPARAMETERS
+#
+##########################
+
 batch_size = 32
 block_size = 8 # Context length
 learning_rate = 1e-3
+max_iters = 6000
+eval_interval = 300
+eval_iters = 200
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-
+##########################
+#
+# Data
+#
+##########################
 with open("data/input.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 chars = sorted(list(set(text)))
-print(chars)
 vocab_size = len(chars)
-print(vocab_size)
 
 # Encoding and Decoding a string to integers and back
 stoi = { ch:i for i, ch in enumerate(chars)}
 itos = { i:ch for i, ch in enumerate(chars)}
-print(stoi)
-print(itos)
-
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: ''.join([itos[elem] for elem in l])
 
-print("Encoding \n")
-print(encode("Before we proceed any further, hear me speak."))
-
-print("Decoding \n")
-print(decode([14, 43, 44, 53, 56, 43, 1, 61, 43, 1, 54, 56, 53, 41, 43, 43, 42, 1, 39, 52, 63, 1, 44, 59, 56, 58, 46, 43, 56, 6, 1, 46, 43, 39, 56, 1, 51, 43, 1, 57, 54, 43, 39, 49, 8]))
-
-
+# Dataset
 data = torch.tensor(encode(text), dtype=torch.long)
 print(data.shape)
+
+# Train and Validation Split
 n = int(0.9 * len(data))
 train_data = data[:n]
 val_data = data[n:]
 
+
+##########################
+#
+# DATALOADER
+#
+##########################
+
+# Get a Batch of Data
 def get_batch(split):
     data = train_data if split == "train" else val_data
     idx = torch.randint(len(data) - block_size, (batch_size,))
@@ -69,4 +81,122 @@ def get_batch(split):
     x, y = x.to(device), y.to(device)
     return x, y
 
-get_batch("train")
+@torch.no_grad()
+def estimate_losses():
+    out = {}
+    # Put Model in Eval Mode
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    # Put Model back in Train Mode
+    model.train()
+    return out
+
+
+class BigramLanguageModel(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, targets=None):
+        # (batch_size, context/block_size, Channels) --> (B, T, C)
+        # idx and targets will be (B, T) integers
+        logits = self.token_embedding(idx)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B * T, C) # For cross entropy calculation
+            target = targets.view(B * T)
+            loss = F.cross_entropy(logits, target)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        """
+        Generates the new/next character.
+
+        idx is same as above, made of (B, T)
+
+        As mentioned above, for BiGram, we only look at current character, whose
+        target is next character.
+        (B, T)
+        x = [14, 43, 44, 53, 56, 43, 1, 61]
+        y = [43, 44, 53, 56, 43, 1, 61, 43]
+
+        14 --> 43
+        43 --> 44
+        44 --> 53
+
+        After, getting embeddings, (B, T, C), where each character's encoding is a
+        64 bit vector.
+
+        Here, we focus only one previous time step. That is to predict 'n' timestep
+        we only look at (n - 1)
+
+        In general in transformers, we look from 1 ... (n-1)
+        """
+
+        for _ in range(max_new_tokens):
+            # Get Model Predictions
+            logits, loss = self(idx)
+
+            # Focus only on the last time step
+            logits = logits[:, -1, :] # (B, C)
+
+            probs = F.softmax(logits, dim=1)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1) #(B, 1)
+
+            # Append sampled idx_next to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
+
+        return idx
+
+##########################
+#
+# TRAINING
+#
+##########################
+
+model = BigramLanguageModel(vocab_size=vocab_size)
+"""
+# model a is in CPU
+device = torch.device('cuda:0')
+b = a.to(device)
+# a and b are in GPU
+# a and b point to the same model
+"""
+m = model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+
+
+for iter in range(max_iters):
+    # Evaluate Train and Validation Losses after certain iterations
+    if iter % eval_interval == 0:
+        losses = estimate_losses()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    xb, yb = get_batch('train')
+
+    # Forward Pass, evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+##########################
+#
+# SAMPLE GENERATION
+#
+##########################
+
+context = torch.zeros((1, 1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
